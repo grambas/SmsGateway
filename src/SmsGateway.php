44bb6ed80937de034d690dc7b76f1a449a2faff4
Mindaugas Milius <milukasm@gmail.com>
2019-01-12 17:41:19 +0100

    Update SmsGateway.php

--- src/SmsGateway.php
+++ src/SmsGateway.php
@@ -4,48 +4,80 @@ use GuzzleHttp\Client;
 
 class SmsGateway
 {
-	protected $client;
-	
+    protected $client;
+
     public function __construct($token, $token_secret)
     {
 
-		$stack = \GuzzleHttp\HandlerStack::create();
-		$oauth_middleware = new \GuzzleHttp\Subscriber\Oauth\Oauth1([
-		    'consumer_key'    => 'Create-an-API-Key',
-		    'consumer_secret' => 'GoGenerateAnApiKeyAndSecret',
-		    'token'           => $token,
-		    'token_secret'    => $token_secret
-		]);
-		$stack->push($oauth_middleware);
+        $stack = \GuzzleHttp\HandlerStack::create();
+        $oauth_middleware = new \GuzzleHttp\Subscriber\Oauth\Oauth1([
+            'consumer_key' => $token,
+            'consumer_secret' => $token_secret,
+            'token' => '',
+            'token_secret' => ''
+        ]);
 
-		$this->client = new \GuzzleHttp\Client([
-		    'base_uri' => 'https://gatewayapi.com/rest/',
-		    'handler'  => $stack,
-		    'auth'     => 'oauth'
-		]);
+        $stack->push($oauth_middleware);
+
+        $this->client = new \GuzzleHttp\Client([
+            'base_uri' => 'https://gatewayapi.com/rest/',
+            'handler' => $stack,
+            'auth' => 'oauth'
+        ]);
 
 
     }
-    
-    public static function saySomething() {
+
+    public static function saySomething()
+    {
         return 'Hello World!';
     }
 
 
-	public function send($sender, $recipient, $message)
-	{
+    /**
+     * Send message
+     * @return array
+     * array:2 [▼
+     * "ids" => array:1 [▼
+     * 0 => 547420628
+     * ]
+     * "usage" => array:3 [▼
+     * "countries" => array:1 [▼
+     * "DE" => 1
+     * ]
+     * "currency" => "EUR"
+     * "total_cost" => 0.0575
+     * ]
+     * ]
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function send($sender, $recipient, $message)
+    {
 
         $req = [
-            'sender'     => $sender,
+            'sender' => $sender,
             'recipients' => [['msisdn' => $recipient]],
-            'message'    => $messag,
+            'message' => $message,
         ];
-            
-		return json_decode( $this->client->post('mtsms', ['json' => $req])->getBody() );
-	}
+        // dd($req);
+        $resposne = $this->client->post('mtsms', ['json' => $req, 'http_errors' => false]);
+        return json_decode($resposne->getBody(), true);
+    }
 
-	public function me()
-	{
-		return json_decode( $this->client->request('GET', 'me')->getBody() );
-	}
-}
+
+    /**
+     * Get basic information about account balance
+     * @return array
+     *      "credit" => 1.5025
+     *       "currency" => "EUR"
+     *       "id" => 6700
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+
+    public function me()
+    {
+
+        $response = $this->client->request('GET', 'me', ['http_errors' => false]);
+
+        return json_decode($response->getBody(), true);
+   
